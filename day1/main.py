da531d13ba4dd423deb6707c6c7a1a59ef859cc0
Truyen08 <thanhtruyen08102004@gmail.com>
2024-07-09 21:48:15 +0700

    update day01

--- day1/main.py
+++ day1/main.py
@@ -1,3 +1,59 @@
-print("hello world! ")
-print("❤️😊")
-print("chao Thanh Truyen")
+# simple-line comment 
+
+'''
+   block/multi-comment
+   in python 
+'''
+
+# valid variable syntax
+xx = 10 
+cau_lenh = "Hello world!"
+_ryo = 3.14
+
+# of you can assign multi-valu for multi vari in 1 line:
+
+x, y, z = 10, 20, 30 
+
+
+# Operator syntax
+
+x = 10
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x ** y)  # tinh' luy~ thua` -> output 10^4
+print( x // y) # chia lay' so' dau` tien, sau thap. phan bo? het'
+
+print("=========")
+
+# assignment operator
+z = 15
+z +=3
+print(z)
+print("------------")
+
+# logical operator
+a = True
+b = False
+
+print(a or b)
+print(a and b)
+print(not b)
+print('-----------')
+
+# membership operator
+
+string = 'hello world!'
+print('h' in string ) # output: true
+print('n' not in string )  #output: true
+print('------?----')
+
+# identify operator
+
+list1 = [1,2,3]
+list2 = [1,2,3]
+print(list1 is list2) #False
+print(list1 is not list2 ) # True
