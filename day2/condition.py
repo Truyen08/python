0145a1a58aba770d458a0e61a1952e5e772a8896
Truyen08 <thanhtruyen08102004@gmail.com>
2024-07-11 09:18:43 +0700

    day2-conditional syntax

---
+++ day2/condition.py
@@ -0,0 +1,19 @@
+# conditional syntax (if    else)
+
+string = 'ThanhTruyen'
+if string == 'ThanhTruyen':
+    print("Hello ThanhTruyen, welcome!")
+else:
+    print("bye")
+
+#and elif is
+
+x = 10
+y = 10
+
+if x > y:
+    print(x, '>', y)
+elif x == y:
+    print(x, "=", y)
+else:
+    print(x, "<", y)
