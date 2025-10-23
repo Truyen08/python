3f6d0846b9d95ecbd9c2fbab1c1334c53878cbf1
Truyen <thanhtruyen08102004@gmail.com>
2025-10-23 17:12:20 +0700

    update exe19

---
+++ exercise/part_02/exe_19.py
@@ -0,0 +1,17 @@
+#mở file --> lưu vào từ điển --> in kết quả + đếm số từ lặp lại
+ftext = open("D:/py_VKU/exercise/chap2_part_02/text.txt", "r", encoding="utf-8")
+tu_dien_cac_tu = {}
+for dong in ftext:
+    danh_sach_tu = dong.split()
+    for tu in danh_sach_tu:
+        tu_dien_cac_tu[tu] = tu_dien_cac_tu.get(tu, 0) + 1
+
+danh_sach = []
+for key, val in tu_dien_cac_tu.items():
+    newtup = (val, key) 
+    danh_sach.append(newtup)
+
+danh_sach = sorted(danh_sach, reverse=True)
+
+for val, key in danh_sach[:10] :
+    print(key, val)
