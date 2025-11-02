de4cf0463409d8f81ca00374a8fd81ddcde0df80
Truyen <thanhtruyen08102004@gmail.com>
2025-11-02 22:50:56 +0700

    update exe21

--- exercise/part_02/exe_21.py
+++ exercise/part_02/exe_21.py
@@ -4,5 +4,6 @@ def Tao_In_DS():
         ds.append(i ** 2)
     print(ds)
     print("5 phần tử đầu + 5 phần tử cuối:", ds[0:5] + ds[-5:])
+    print(f"5 phần tử đầu {ds[0:5]} và 5 phần tử cuối {ds[-5:]}")
 
 Tao_In_DS()
