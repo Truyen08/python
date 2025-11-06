b4bf6c96a8e96fa8279fb6c111e75f8ba0de02e8
Truyen <thanhtruyen08102004@gmail.com>
2025-11-06 16:10:09 +0700

    update exe24

---
+++ exercise/part_02/exe_24/compare.py
@@ -0,0 +1,11 @@
+def so_sanh(s1, s2):
+    len1 = len(s1)
+    len2 = len(s2)
+    
+    if len1 > len2:
+        print("chuoi dai hon la: ", s1)
+    elif len2 > len1:
+        print("chuoi dai hon la: ", s2)
+    else:
+        print(s1)
+        print(s2)
---
+++ exercise/part_02/exe_24/tesst.py
@@ -0,0 +1,8 @@
+from compare import *
+
+print("---> start <-----")
+
+n1 = (input("Nhap. chuoi~ s1: "))
+n2 = (input("Nhap. chuoi~ s2: "))
+
+so_sanh(n1, n2)
