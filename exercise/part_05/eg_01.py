3c7b74b59b10cc96d7dfb7539adf15b54ae020f9
Truyen <thanhtruyen08102004@gmail.com>
2025-11-11 23:22:19 +0700

    exer of numpy array

---
+++ exercise/part_05/eg_01.py
@@ -0,0 +1,11 @@
+# pip install numpy
+
+import numpy as np
+
+
+arr = np.array([1 , 2, 3, 4, 5])
+print("arr :", arr)
+print("Shape of arr:", arr.shape)
+print("Size of arr:", arr.size)
+print("Data type of arr :", arr.dtype)
+print("Number of dimensions of arr:", arr.ndim)     #chiều sâu của mảng (ở đây là mảng 1 chiều)
