868725fb150cf31241aa248bbcad5dbd49c11c88
Truyen <thanhtruyen08102004@gmail.com>
2025-11-16 22:14:37 +0700

    update eg Arithmetic

---
+++ exercise/part_05/eg_12.py
@@ -0,0 +1,11 @@
+#  Numpy Arithmetic Operations
+import numpy as np
+a = np.array ([[0, 3, 1],
+			 [3, 7, 5],
+ 			[6, 4, 8]])
+b = [10, 5, 11]
+
+print("add_array =\n",np.add(a,b))
+print("subtract_array =\n",np.subtract(a,b))
+print("multiply_array =\n",np.multiply(a,b))
+print("divide_array =\n",np.divide(a,b))
