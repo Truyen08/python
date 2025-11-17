2e6e6a87af0ce5450534200c5016cb73407c5e30
Truyen <thanhtruyen08102004@gmail.com>
2025-11-17 22:51:52 +0700

    update np sum

--- exercise/part_05/eg_12.py
+++ exercise/part_05/eg_12.py
@@ -9,3 +9,10 @@ print("add_array =\n",np.add(a,b))
 print("subtract_array =\n",np.subtract(a,b))
 print("multiply_array =\n",np.multiply(a,b))
 print("divide_array =\n",np.divide(a,b))
+
+print("--"*20)
+print("sum_a =",np.sum(a))	#total sum of all elements in a
+print("sum_b =",np.sum(b))	#total sum of all elements in b
+
+print("sum of each column in a =",np.sum(a, axis=0)) #sum of each column
+print("sum of each row in a =",np.sum(a, axis=1)) #sum of each row
