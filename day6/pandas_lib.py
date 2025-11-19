700003ef27233a0f85ccede1049179708a9612b4
Truyen <thanhtruyen08102004@gmail.com>
2025-11-19 22:58:22 +0700

    update file

--- day6/pandas_lib.py
+++ day6/pandas_lib.py
@@ -11,8 +11,17 @@ in pandas has 3 main data structures:
 import pandas as pd
 
 data1 = pd.Series([5,7,5,1,6], name = "seri_example1")
-data2 = pd.Series(["C++","Java","Python"], index = list("CJP"),
+data2 = pd.Series(["C++","Java","Python"], index = list("CJP"), name = "seri_example2")
 
-name = "seri_example2")
 print(data1)
 print(data2)
+
+print("--"*20)
+
+print(data1[0])
+print(data1[3]) 
+
+print("--"*20)
+print(data1[2:4])  #print value from index 2 to 3
+
+print(data2["J"])
