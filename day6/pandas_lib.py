b804c6654aadf97659d088c63edac9d3bc4f0e2b
Truyen <thanhtruyen08102004@gmail.com>
2025-11-20 23:56:20 +0700

    update learn pandas

--- day6/pandas_lib.py
+++ day6/pandas_lib.py
@@ -11,17 +11,38 @@ in pandas has 3 main data structures:
 import pandas as pd
 
 data1 = pd.Series([5,7,5,1,6], name = "seri_example1")
-data2 = pd.Series(["C++","Java","Python"], index = list("CJP"), name = "seri_example2")
+data2 = pd.Series(["C++","Java","Python", "NET"], index = list("CJPN"), name = "seri_example2")
 
 print(data1)
 print(data2)
 
-print("--"*20)
 
+
+
+print("--"*20)
 print(data1[0])
 print(data1[3]) 
+
+data1[2.5] = 9  #add new value at index 2.5
+
+data1.sort_index(inplace=True)  #sort index
+
+data1.reset_index(drop=True)  #reset index
+print(data1)
+
+
+
 
 print("--"*20)
 print(data1[2:4])  #print value from index 2 to 3
 
+
+
+
 print(data2["J"])
+
+print(data2.str.count('a'))
+print(data2.str.count('Python'))
+
+print(data2.str.upper())
+print(data2.str.replace("Java", "C#"))
